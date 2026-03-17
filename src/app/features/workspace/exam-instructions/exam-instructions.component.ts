68ace2b3b4757833f4cc5ae9940ac0bacc5f5eb7
ajitrajbharcareer <ajitrajbhar.career@gmail.com>
2026-03-17 21:52:59 +0530

    updated instructions page

--- src/app/features/workspace/exam-instructions/exam-instructions.component.ts
+++ src/app/features/workspace/exam-instructions/exam-instructions.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-exam-instructions',
   templateUrl: './exam-instructions.component.html',
   styleUrls: ['./exam-instructions.component.scss'],
+  imports:[RouterModule]
 })
 export class ExamInstructionsComponent  implements OnInit {
 
